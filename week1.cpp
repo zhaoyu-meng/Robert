cd38b3c355ca7c808682631b9857cc6057fcd7c8
zhaoyu-meng <159064000+zhaoyu-meng@users.noreply.github.com>
2024-02-08 11:29:46 +0000

    week1.cpp

--- week1.cpp
+++ week1.cpp
@@ -1,62 +1,50 @@
+//Calcualtion of photon energies of electron transition from Bohr model
+//week one course work
+//Zhaoyu Meng, student ID:10832580, 8 JAN 2024
 #include<iostream>
 #include<string>
 #include<math.h>
-
-
-int is_valid(std::string text_out, std::string str1, std::string str2) 
+std::string is_valid(std::string text_out, std::string str_1, std::string str_2)
 {
-  std::string temp; 
-  std::cout<<text_out; 
+  //To check if input is valid for string input
+  std::string temp;
+  std::cout<<text_out;
   std::cin>>temp;
-
-  while(std::cin.fail() || (temp != str1 && temp != str2)) 
-  {    
-    std::cin.clear(); 
-    std::cin.ignore(); 
-    std::cout<<"enter valid value: "; 
+  while(std::cin.fail() || (temp != str_1 && temp != str_2))
+  {
+    std::cin.clear();
+    std::cin.ignore();
+    std::cout<<"enter valid value: ";
   // Clear fail bit and ignore bad input
-
     std::cin>>temp;
   }
-
-  if (temp == str1)
-  {
-    return 0;
-  }
-  if (temp == str2)
-  {
-    return 1;
-  }
-  return 0;
+  return temp;
 }
-
-int is_valid(std::string text_out) 
+int is_valid(std::string text_out)
 {
-  int temp; 
+  //To check if input is valid for int input
+  int temp;
   {
-  std::cout<<text_out; 
+  std::cout<<text_out;
   std::cin>>temp;
-
-  while(std::cin.fail()) 
-  {    
-    std::cin.clear(); 
-    std::cin.ignore(); 
-    std::cout<<"enter valid value: "; 
+  while(std::cin.fail())
+  {
+    std::cin.clear();
+    std::cin.ignore();
+    std::cout<<"enter valid value: ";
     // Clear fail bit and ignore bad input
-
       std::cin>>temp;
   }
   }
   return temp;
 }
-
 int main()
 {
-
+  //main function
   std::cout<<"Eij = 13.6*Z^2*(nj^(-2)-ni^(-2))eV"<<std::endl;
-   int loop;
-   loop = 1;
-   while (loop == 1)
+   std::string loop;
+   loop="y";
+   while(loop=="y")
   {
     int    Z;
     Z=is_valid("Z=");
@@ -64,22 +52,20 @@ int main()
     n_i=is_valid("n_i=");
     int    n_j;
     n_j=is_valid("n_j=");
-    int    unit;
-    unit=is_valid("eV or J\n", "eV", "J");
+    std::string   unit;
+    unit=is_valid("unit in eV or J\n", "eV", "J");
     double energy;
-    energy=13.6*Z*Z*(pow(n_j,-2)-pow(n_i,-2));
-
-    if (unit==0)
+    energy=13.6*Z*Z*(pow(n_j,-2)-pow(n_i,-2));//compote energy in eV
+    if (unit=="eV")
     {
       std::cout<<energy<<"eV"<<std::endl;
     }
-    if (unit==1)
+    if (unit=="J")
     {
-      energy=energy*1.6*pow(10,-19);
+      energy=energy*1.6*pow(10,-19);//convert energy into J
       std::cout<<energy<<"J"<<std::endl;
     }
-
-    loop=is_valid("continue?\nn to stop and y to continue\n", "n", "y");
+    loop=is_valid("continue?\nn to stop and y to continue\n","n","y");
   }
   return 0;
 }
